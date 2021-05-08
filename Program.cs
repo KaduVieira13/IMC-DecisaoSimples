65d7aa296b755707e3993d2c77608f70b0bbb2f0
Diego Neri <diego@diegoneri.com>
2021-05-07 22:12:40 -0300

    Formata o código fonte

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace IMC_DecisaoSimples
 {
@@ -9,44 +9,44 @@ namespace IMC_DecisaoSimples
 
             Console.WriteLine(" --- Índice de Massa Corporal ---\n");
 
-            double altura,peso,resultado;
+            double altura, peso, resultado;
 
-             Console.Write("Digite sua altura: ");
-             altura = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+            Console.Write("Digite sua altura: ");
+            altura = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
 
-             Console.Write("Digite seu peso: ");
-             peso = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite seu peso: ");
+            peso = Convert.ToDouble(Console.ReadLine());
 
-             resultado = peso / Math.Pow(altura, 2);
+            resultado = peso / Math.Pow(altura, 2);
 
-             int novoResultado = Convert.ToInt32(resultado);
+            int novoResultado = Convert.ToInt32(resultado);
 
-            if (novoResultado <17)
+            if (novoResultado < 17)
             {
-               Console.ForegroundColor = ConsoleColor.DarkYellow;
-               Console.WriteLine($"{novoResultado:N1}Kg/m² Você está muito abaixo do peso ideal.");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"{novoResultado:N1}Kg/m² Você está muito abaixo do peso ideal.");
             }
 
-            else if (novoResultado <=18.49)
+            else if (novoResultado <= 18.49)
             {
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine($"{novoResultado:N1}Kg/m² abaixo do peso ideal.");
             }
-            else if (novoResultado <=24.99)
+            else if (novoResultado <= 24.99)
             {
                 Console.WriteLine($"{novoResultado:N1}Kg/m² peso normal");
             }
-            else if (novoResultado <=29.99)
+            else if (novoResultado <= 29.99)
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"{novoResultado:N1}Kg/m² você está acima do peso.");
             }
-            else if (novoResultado <=34.99)
+            else if (novoResultado <= 34.99)
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"{novoResultado:N1}Kg/m² Obesidade 1.");
             }
-            else if (novoResultado <=39.99)
+            else if (novoResultado <= 39.99)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"{novoResultado:N1}Kg/m² Obesidade 2 Severa");
@@ -54,8 +54,8 @@ namespace IMC_DecisaoSimples
 
             else
             {
-               Console.ForegroundColor = ConsoleColor.Red;
-               Console.WriteLine($"{novoResultado:N1}Kg/m² Obesidade tipo 3 Mórbida");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{novoResultado:N1}Kg/m² Obesidade tipo 3 Mórbida");
             }
 
             Console.ResetColor();
